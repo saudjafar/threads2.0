b367268aa213c48a6302d29746320f41ddd4359e
Mohammed Saud Jaafar <saudjafar71@gmail.com>
2023-10-13 01:09:29 +0530

    fixed onboarding page functionality

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,15 @@
 import AccountProfile from "@/app/components/forms/AccountProfile";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 
 async function Page() {
     const user = await currentUser();
+    if (!user) return null; // to avoid typescript warnings
 
-    const userInfo = {};
+    const userInfo = await fetchUser(user.id);
+    if (userInfo?.onboarded) redirect("/");
+
 
     const userData = {
         id: user?.id,
